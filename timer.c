59b97fdb54b8cf508b5218a47a532f77d848d21d
FUKUMOTO Yuki <rk76fewf@gmail.com>
2023-03-09 14:54:57 +0900

    [Update] T2_setup関数の引数を追加
    引数からタイマの設定を変更できるように改良

--- timer.c
+++ timer.c
@@ -1,24 +1,50 @@
 #include <xc.h>
 #include "user.h"
 
-void T2_setup(void);
+void T2_setup(int, int);
 
 void TIMER_setup(void)
 {
-    T2_setup();
+    // (1/16000000) * prescaler * period
+
+    // (1/16000000) * 256 * 1250 = 0.02s
+    T2_setup(256, 1250);
 
     return;
 }
 
-void T2_setup()
+void T2_setup(int prescaler, int period)
 {
     T2CON = 0x0000;
-    T2CONbits.TCKPS = 0b11; // 1:256 prescaler
 
-    PR2 = 1250; // 0.02s period
-    _T2IE = 1;  // enable interrupt
-    _T2IP = 6;  // interrupt priority
-    _T2IF = 0;  // clear interrupt flag
+    unsigned short int TCKPS = 0;
+    switch (prescaler)
+    {
+    case 1:
+        TCKPS = 0b00;
+        break;
+    case 8:
+        TCKPS = 0b01;
+        break;
+    case 64:
+        TCKPS = 0b10;
+        break;
+    case 256:
+        TCKPS = 0b11;
+        break;
+    default:
+        return;
+    }
+
+    if (period > 65535)
+        return;
+
+    T2CONbits.TCKPS = TCKPS; // 1:256 prescaler
+
+    PR2 = period; // period
+    _T2IE = 1;    // enable interrupt
+    _T2IP = 6;    // interrupt priority
+    _T2IF = 0;    // clear interrupt flag
 
     TMR2 = 0x0000;     // reset timer
     T2CONbits.TON = 1; // start timer
