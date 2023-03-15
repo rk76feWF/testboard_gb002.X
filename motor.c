d2bc014b495603a3fe75f5fc8c46ad8318b263e2
FUKUMOTO Yuki <rk76fewf@gmail.com>
2023-03-16 02:19:08 +0900

    [Clean] 変数名で余計な部分を削除
    dir_s1, dir_s2 -> dir1, dir2に変更

--- motor.c
+++ motor.c
@@ -31,7 +31,7 @@ void MOTOR_setup(void)
 
 int motor(int number, double velocity)
 {
-    bool dir_s1, dir_s2;
+    bool dir1, dir2;
 
     // velocity: -1.0 ~ 1.0
     if (velocity > 1.0 || velocity < -1.0)
@@ -39,32 +39,32 @@ int motor(int number, double velocity)
 
     // direction
     if (velocity > 0.0)
-        dir_s1 = 1, dir_s2 = 0;
+        dir1 = 1, dir2 = 0;
     else if (velocity == 0.0)
-        dir_s1 = 0, dir_s2 = 0;
+        dir1 = 0, dir2 = 0;
     else
-        dir_s1 = 0, dir_s2 = 1;
+        dir1 = 0, dir2 = 1;
 
     switch (number)
     {
     case 1:
-        M1S1 = dir_s1;
-        M1S2 = dir_s2;
+        M1S1 = dir1;
+        M1S2 = dir2;
         OC1R = fabs(velocity) * OC1RS;
         break;
     case 2:
-        M2S1 = dir_s1;
-        M2S2 = dir_s2;
+        M2S1 = dir1;
+        M2S2 = dir2;
         OC2R = fabs(velocity) * OC2RS;
         break;
     case 3:
-        M3S1 = dir_s1;
-        M3S2 = dir_s2;
+        M3S1 = dir1;
+        M3S2 = dir2;
         OC3R = fabs(velocity) * OC3RS;
         break;
     case 4:
-        M4S1 = dir_s1;
-        M4S2 = dir_s2;
+        M4S1 = dir1;
+        M4S2 = dir2;
         OC4R = fabs(velocity) * OC4RS;
         break;
     default:
